5ebaf179e42a556694d7764f37f98d5501199d53
narekhovhannisyan <narekgilmour@gmail.com>
2021-03-17 17:58:22 +0400

    src: in index get rid of same credential version.

--- src/index.js
+++ src/index.js
@@ -3,42 +3,33 @@ const NodeSSH = require('node-ssh')
 const { getValueByAsync, store } = require('./util/cache').SINGLETON
 
 /**
- * @typedef {import('./types/ssh').TNodeSSH} TNodeSSH
+ * @typedef {import('node-ssh')} NodeSSH
+ * @typedef {import('./types/ssh').TSSHOptions} TSSHOptions
  * @typedef {import('./types/ssh').TSSHConnectionOptions} TSSHConnectionOptions
- * @typedef {import('./types/ssh').TSSHConnectionOptionsWithDomain} TSSHConnectionOptionsWithDomain
  */
 
 /**
- * Provides new SSH connection if there is no such one, otherwise creates new one and stores in cache.
- * @param {TSSHConnectionOptionsWithDomain} sshOptionsWithDomain SSH connection options.
- * @return {Promise.<TNodeSSH>} SSH connection to desired remote.
+ * SSH connection wrapper which uses cache for storing connections.
+ * @param {TSSHConnectionOptions} param0
+ * @return {Promise.<NodeSSH>} SSH Session.
  */
-const connect = ({ domainOrIP, connectionExpire, ...sshOptions }) =>
-  getValueByAsync(domainOrIP).then((connection) => {
-    if (!connection) {
-      return new NodeSSH()
-        .connect(sshOptions)
-        .then(store(domainOrIP, connectionExpire))
-    }
+const SSHCache = ({ domainOrIP, connectionExpire, ...sshOptions }) => {
+  /**
+   * Provides new SSH connection if there is no such one, otherwise creates new one and stores in cache.
+   * @param {String} domainOrIP The remote server domain.
+   * @return {Promise.<NodeSSH>} SSH connection.
+   */
+  const connect = (domainOrIP) => getValueByAsync(domainOrIP)
+    .then((connection) => {
+      if (!connection) {
+        return new NodeSSH().connect(sshOptions)
+          .then(store(domainOrIP, connectionExpire))
+      }
 
-    return connection
-  })
+      return connection
+    })
 
-/**
- * Reusable Node SSH connection for different remotes with same connection credentials.
- * @param {TSSHConnectionOptions} sshOptions The connection options.
- * @return {(domainOrIP: String) => Promise.<TNodeSSH>} SSH Session.
- */
-const NodeSSHExtraForSameCredentials = (sshOptions) => (domainOrIP) => connect({ ...sshOptions, domainOrIP })
-
-/**
- * Reusable Node SSH connection.
- * @param {TSSHConnectionOptionsWithDomain} sshOptionsWithDomain
- * @returns {Promise.<TNodeSSH>}
- */
-const NodeSSHExtra = (sshOptionsWithDomain) => connect(sshOptionsWithDomain)
-
-module.exports = {
-  NodeSSHExtraForSameCredentials,
-  NodeSSHExtra
+  return connect(domainOrIP)
 }
+
+module.exports = SSHCache
