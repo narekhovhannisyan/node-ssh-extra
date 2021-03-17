5c1e8f7e20af4a7ce7b835a2c12994fc69ee6590
narekhovhannisyan <narekgilmour@gmail.com>
2021-03-17 17:57:53 +0400

    types: in ssh remove redundant ssh types.

--- src/types/ssh.js
+++ src/types/ssh.js
@@ -1,34 +1,9 @@
 /**
- * Exec command argument type.
- * @typedef {Object} TExecCommandArgs
- * @property {String} command
- * @property {Object} options
- * @private
- */
-
-/**
- * SSH execCommand response stream.
- * @typedef {Object} TStreamResponse
- * @property {String} stdout
- * @property {String} stderr
- * @property {Number} code
- * @private
- */
-
-/**
- * Node SSH instance.
- * @typedef {Object} TNodeSSH
- * @property {() => Promise.<TStreamResponse>} execCommand
- * @property {() => void} dispose
- * @property {() => any} connect
- * @property {Function} putDirectory
- * @property {Function} putFiles
- * @property {Function} putFile
- * @property {Function} getFile
- * @property {Function} exec
- * @property {Function} mkdir
- * @property {Function} requestSFTP
- * @property {Function} requestShell
+ * @typedef {Object} TSSHOptions
+ * @property {String} privateKey
+ * @property {String} host
+ * @property {String} passphrase
+ * @property {String} username
  */
 
 /**
@@ -39,10 +14,7 @@
  * @property {String} passphrase Passphrase for ssh connection.
  * @property {String} username User name for ssh connection.
  * @property {Number} connectionExpire Milliseconds to expire ssh connections.
- */
-
-/**
- * @typedef {TSSHConnectionOptions & {domainOrIP: String}} TSSHConnectionOptionsWithDomain
+ * @property {String} domainOrIP Domain or IP address to connect.
  */
 
 module.exports = {}
